5da3eb98a752d08f90fe41c817563e77587e9c00
Randy Aull <randyau@microsoft.com>
2023-01-08 03:35:23 -0800

    2015 Day 20

---
+++ 2015/day_20_2/src/main.rs
@@ -0,0 +1,76 @@
+fn count_presents(house: u64) -> u64 {
+    let mut presents = 0;
+    //println!("{}", house);
+    let mut divs = divisors::get_divisors(house);
+    //
+    // Address bug in divisors.
+    //
+
+    if house != 2 {
+        //assert!(!divs.contains(&house));
+        divs.push(house);
+    }
+
+    divs.dedup();
+
+    //
+    // 1 isn't included
+    //
+    presents += 11;
+    let min_elf = house / 50;
+
+    for d in divs {
+        if d >= min_elf {
+            //println!("  {}", d);
+            presents += 11 * d;
+        }
+    }
+    println!("house: {}, presents: {}", house, presents);
+    presents
+}
+fn solve_puzzle(target: u64) -> u64 {
+    let mut result = 0;
+    let max_house = target / 10;
+    for i in 1..max_house {
+        if count_presents(i) >= target {
+            return i;
+        }
+    }
+    0
+}
+
+fn main() {
+    //
+    // !!!! Update with the expected result for the sample data !!!!
+    //
+    let expected_sample_output = 8;
+
+    //
+    // Print the specific puzzle info
+    //
+    let (day, part) = sscanf::sscanf!(env!("CARGO_PKG_NAME"), "day_{}_{}", u32, u32).unwrap();
+    println!("Day {} : Part {}!", day, part);
+
+    //
+    // Solve for the sample input
+    //
+    println!("Testing Sample Data:");
+    let result = solve_puzzle(140);
+    println!("Sample Result : {}", result);
+
+    //
+    // Check if the sample input matches the expected result
+    //
+    if result != expected_sample_output {
+        println!("Wrong Answer!!!!! {}", result);
+        return;
+    }
+
+    //
+    // Solve for the real input, only in the case that the result
+    // for the sample input was correct
+    //
+    println!("Testing Real Data:");
+    let result = solve_puzzle(29000000);
+    println!("Real Result : {}", result);
+}
