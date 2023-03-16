52a0ee9f6dc1a4af981a251e918917b1503aab04
Randy Aull <randyau@microsoft.com>
2023-03-15 17:22:01 -0700

    2019 18 part 1

---
+++ Python/2019/18/solution.py
@@ -0,0 +1,209 @@
+# advent of code 2019
+# https://adventofcode.com/2019
+# day 18
+
+from collections import deque
+import copy
+from Map2D import Map2D
+from aocpuzzle import *
+from mapgraph import *
+
+
+class Puzzle(AoCPuzzle):
+    def __init__(self, lines, is_test=False):
+        AoCPuzzle.__init__(self, lines, is_test)
+        self.map = Map2D()
+        self.map.fill_from_lines(lines)
+        self.map.print()
+        self.points_of_interest = {}
+        self.keys = set()
+        self.doors = set()
+        self.graph = Map2DGraph(self.map, r"abcdefghijklmnopqrstuvwxyz@ABCDEFGHIJKLMNOPQRSTUVWXYZ.")
+        self.graphs = {}
+        self.shortest_path = 9999999999999
+
+    def get_next_key(self, pos, found_keys, graph, steps):
+        print(found_keys, steps, self.shortest_path)
+        keys_left = self.keys - found_keys
+        # print(keys_left)
+        if len(keys_left) == 0:
+            if steps < self.shortest_path:
+                self.shortest_path = steps
+                print(steps)
+        if steps > self.shortest_path:
+            return
+
+        keys = []
+        for key in keys_left:
+            if nx.has_path(graph, pos, self.points_of_interest[key]):
+                distance = nx.shortest_path_length(graph,
+                                                   pos, self.points_of_interest[key])
+                keys.append((key, distance))
+        keys.sort(key=lambda x: x[1])
+
+        for (key,distance) in keys:
+            # print(distance)
+            new_graph = copy.deepcopy(graph)
+            if key.upper() in self.points_of_interest:
+                new_graph.make_coord_valid(
+                    self.points_of_interest[key.upper()])
+            self.get_next_key(self.points_of_interest[key], found_keys | set(
+                key), new_graph, steps + distance)
+            
+    def get_graph_for_keys(self, keys):
+        key_hash = list(keys)
+        key_hash.sort()
+        key_hash = tuple(key_hash)
+        if key_hash in self.graphs:
+            return self.graphs
+
+        graph = copy.deepcopy(self.graph)
+        remaining_keys = self.keys - keys
+        for key in remaining_keys:
+            graph.remove_node()
+
+    def bfs_search(self):
+        keys_left = self.keys
+        pos = self.points_of_interest["@"]
+        history = {}
+        queue = []
+        queue.append((pos, keys_left, 0, []))
+        while(len(queue)):
+            
+            queue.sort(key=lambda x: x[2], reverse=True)
+            (pos, keys_left, steps, path) = queue.pop()
+            keys_left_hash = list(keys_left)
+            keys_left_hash.sort()
+            keys_left_hash = tuple(keys_left_hash)
+            if (pos, keys_left_hash) in history:
+                if steps >= history[(pos, keys_left_hash)]:
+                    continue
+            history[(pos, keys_left_hash)] = steps
+            print(keys_left, steps)
+            if len(keys_left) == 0:
+                return steps, path
+            
+            graph = copy.deepcopy(self.graph)
+            keys_found = self.keys - keys_left
+            for key in keys_found:
+                if (key.upper() in self.points_of_interest):
+                    graph.make_coord_valid(self.points_of_interest[key.upper()])
+
+            for key in keys_left:
+                #print("trying next key ", key)
+                if nx.has_path(graph, pos, self.points_of_interest[key]):
+                    #print("found path to ", key)
+                    distance = nx.shortest_path_length(graph, pos, self.points_of_interest[key])
+                    queue.append((self.points_of_interest[key], keys_left - set(key), steps + distance, path + [key]))
+
+        assert(False)
+    def get_pois(self):
+        for coord in self.map:
+            value = self.map[coord]
+            if value == "." or value == "#":
+                continue
+            self.points_of_interest[value] = coord
+            if value in "abcdefghijklmnopqrstuvwxyz":
+                self.keys.add(value)
+            if value in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                self.doors.add(value)
+
+    def part1a(self):
+        self.get_pois()
+        print(self.points_of_interest)
+        print(self.keys)
+        steps, path = self.bfs_search()
+        print(steps, path)
+        #self.get_next_key(self.points_of_interest["@"], set(), self.graph, 0)
+        return steps
+                
+
+    def get_paths_from_point(self, poi):
+        pos = self.points_of_interest[poi]
+        paths = {}
+
+        dist = 0
+        queue = deque()
+        history = {}
+        queue.append((pos, dist))
+        while (len(queue)):
+            (pos, dist) = queue.popleft()
+            if pos in history:
+                continue
+            history[pos] = dist
+            value = self.map[pos]
+            if value != "." and value != poi:
+                paths[value] = dist
+                continue
+            for n in pos.neighbors():
+                if self.map[n] == "#":
+                    continue
+                queue.append((n, dist+1))
+        return paths
+
+    def build_reduced_graph(self):
+        reduced_graph = nx.Graph()
+        for p in self.points_of_interest:
+            paths = self.get_paths_from_point(p)
+            for d in paths:
+                reduced_graph.add_edge(p, d, weight=paths[d])
+
+        return reduced_graph
+
+
+        
+        
+
+                
+
+    def bfs_b(self, reduced_graph):
+        keys_left = self.keys
+        history = {}
+        queue = []
+        print(reduced_graph)
+        queue.append(("@", keys_left, 0, []))
+        print(reduced_graph["@"])
+        while(len(queue)):
+            queue.sort(key=lambda x: x[2], reverse=True)
+            (poi, keys_left, steps, path) = queue.pop()
+            
+            if len(keys_left) == 0:
+                return (steps, path)
+            #print(reduced_graph[poi])
+            keys_hash = list(keys_left)
+            keys_hash.sort()
+            keys_hash = tuple(keys_hash)
+            #print (keys_hash, path)
+            if (poi, keys_hash) in history:
+                if history[(poi, keys_hash)] <= steps:
+                    continue
+            history[(poi, keys_hash)] = steps
+            
+            for n in reduced_graph[poi]:
+                #print(n, reduced_graph[poi][n]["weight"])
+                if n in self.doors:
+                    if n.lower() in keys_left:
+                        continue                
+                #print(n)
+                queue.append((n, keys_left - set(n), steps + reduced_graph[poi][n]["weight"], path + [n]))
+
+
+
+        
+
+
+    def part1b(self):
+        self.get_pois()
+        reduced_graph = self.build_reduced_graph()
+        (steps, path) = self.bfs_b(reduced_graph)
+        print(steps, path)
+        return steps
+
+        
+        
+
+    def part1(self):
+        return self.part1b()
+
+    def part2(self):
+        pass
--- Python/2019/aoctoolbox/Map2D.py
+++ Python/2019/aoctoolbox/Map2D.py
@@ -3,24 +3,31 @@ from Coord2D import *
 
 import sys
 
+
 class PerimeterType(Enum):
     NONE = 1
     RECTANGLE = 2
 
+
 class Map2DOverlay:
-    def __init__(self, map, altitude):    
+    def __init__(self, map, altitude):
         self.attached_map = map
         self.altitude = altitude
         self.map = {}
+
     def clear(self):
         self.map.clear()
-    def __getitem__(self, k):        
+
+    def __getitem__(self, k):
         return self.map.get(k, None)
+
     def __setitem__(self, k, value):
         self.map[k] = value
+
     def __contains__(self, k):
         return k in self.map
-    
+
+
 class PointerOverlay(Map2DOverlay):
 
     def __init__(self, map):
@@ -31,89 +38,90 @@ class PointerOverlay(Map2DOverlay):
     def __set_item__(self, k, value):
         self.coord = k
         self.value = value
-    
+
     def __getitem__(self, k):
         if k == self.coord:
             return self.value
         else:
             return None
-            
+
     def clear(self):
         self.coord = None
         self.value = None
 
     def __contains__(self, k):
         return k == self.coord
-        
+
+
 class Map2D:
-    def __init__(self, default = 0):
-        
+    def __init__(self, default=None):
+
         self.default = default
-        
+
         self.perimeter_type = PerimeterType.NONE
         self.overlays = []
         self.pointer_overlay = None
         self.clear()
+
     def clear(self):
         self.map = {}
         self.minx = sys.maxsize
         self.maxx = -sys.maxsize - 1
         self.miny = sys.maxsize
-        self.maxy = -sys.maxsize -1 
+        self.maxy = -sys.maxsize - 1
         for overlay in self.overlays:
             overlay.clear()
 
     def __iter__(self):
-        return self.map.__iter__()    
-    
+        return self.map.__iter__()
+
     def get_new_overlay(self, altitude):
         return Map2DOverlay(self, altitude)
-    
+
     def add_overlay(self, altitude):
-    
+
         new_overlay = self.get_new_overlay(altitude)
         self.overlays.append(new_overlay)
         self.overlays.sort(key=lambda x: x.altitude, reverse=True)
         return new_overlay
-    
+
     def remove_overlay(self, overlay):
         del self.overlays[overlay]
 
     def _get_new_pointer_overlay(self):
-        return PointerOverlay(self)        
-    
+        return PointerOverlay(self)
+
     def set_pointer(self, k, value):
         if not self.pointer_overlay:
             self.pointer_overlay = self._get_new_pointer_overlay()
 
         pointer = self.get_pointer_overlay()
         pointer[k] = value
-        
-        
+
     def fill_from_lines(self, lines):
         y = 0
         for line in lines:
             x = 0
             for c in line:
-                self[Coord2D(x,y)] = c
+                self[Coord2D(x, y)] = c
                 x += 1
             y += 1
 
     def is_out_of_bounds(self, coord):
-        if self.perimeter_type == PerimeterType.RECTANGLE:        
-            if (coord.x <= (self.perimeter_minx)) or (coord.x >= (self.perimeter_maxx)) or (coord.y <= (self.perimeter_miny)) or (coord.y >= (self.perimeter_maxy)):                
+        if self.perimeter_type == PerimeterType.RECTANGLE:
+            if (coord.x <= (self.perimeter_minx)) or (coord.x >= (self.perimeter_maxx)) or (coord.y <= (self.perimeter_miny)) or (coord.y >= (self.perimeter_maxy)):
                 return True
         return False
 
-    def __getitem__(self, k):        
+    def __getitem__(self, k):
         if self.is_out_of_bounds(k):
             return self.perimeter_value
         return self.map.get(k, self.default)
 
-    def __setitem__(self, k, value):        
-        if self.is_out_of_bounds(k):            
+    def __setitem__(self, k, value):
+        if self.is_out_of_bounds(k):
             return
-    
+
         self.minx = min(self.minx, k.x)
         self.maxx = max(self.maxx, k.x)
         self.miny = min(self.miny, k.y)
@@ -124,30 +132,29 @@ class Map2D:
         if self.pointer_overlay:
             if coord in self.pointer_overlay:
                 return self.pointer_overlay[coord]
-            
+
         for overlay in self.overlays:
             if coord in overlay:
                 return overlay[coord]
         return self[coord]
-        
 
     def __hash__(self):
         return hash(frozenset(self.map.items()))
-    
+
     def __len__(self):
         return len(self.map)
-    
+
     def get_width(self):
         if self.perimeter_type == PerimeterType.RECTANGLE:
             return self.perimeter_maxx - self.perimeter_minx
         return (self.maxx + 1) - self.minx
-    
+
     def get_height(self):
         if self.perimeter_type == PerimeterType.RECTANGLE:
             return self.perimeter_maxy - self.perimeter_miny
         return (self.maxy + 1) - self.miny
-    
-    def print(self, offset=0, max_y_size = 1000, max_x_size = 1000, default="."):
+
+    def print(self, offset=0, max_y_size=1000, max_x_size=1000, default="."):
 
         print()
         y_range = range(self.miny-offset, self.maxy + 1 + offset)
@@ -160,16 +167,15 @@ class Map2D:
             return
         for y in y_range:
             for x in x_range:
-                coord = Coord2D(x,y)
-                #if coord in self.map.keys():
+                coord = Coord2D(x, y)
+                # if coord in self.map.keys():
                 print(self.get_top(coord), end="")
-                #else:
+                # else:
                 #    print(default, end="")
             print()
-    
 
     def set_auto_rectangle_perimeter(self, value, offset=1):
-        self.perimeter_type = PerimeterType.RECTANGLE        
+        self.perimeter_type = PerimeterType.RECTANGLE
         self.perimeter_value = value
         self.perimeter_minx = self.minx - offset
         self.perimeter_maxx = self.maxx + offset
@@ -177,14 +183,14 @@ class Map2D:
         self.perimeter_maxy = self.maxy + offset
 
     def set_rectangle_perimeter(self, value, minx, miny, maxx, maxy):
-        self.perimeter_type = PerimeterType.RECTANGLE        
+        self.perimeter_type = PerimeterType.RECTANGLE
         self.perimeter_value = value
         self.perimeter_minx = minx
         self.perimeter_maxx = maxx
         self.perimeter_miny = miny
         self.perimeter_maxy = maxy
 
-    def rectangle_coords(self, offset = 0):
+    def rectangle_coords(self, offset=0):
         if self.perimeter_type == PerimeterType.RECTANGLE:
             minx = self.perimeter_minx + 1
             miny = self.perimeter_miny + 1
@@ -198,9 +204,9 @@ class Map2D:
 
         for y in range(miny - offset, maxy+1 + offset):
             for x in range(minx - offset, maxx + 1 + offset):
-                yield Coord2D(x,y)
+                yield Coord2D(x, y)
 
-    def is_boundary(self, coord, offset = 0):
+    def is_boundary(self, coord, offset=0):
         if self.perimeter_type == PerimeterType.RECTANGLE:
             minx = (self.perimeter_minx + 1) - offset
             miny = (self.perimeter_miny + 1) - offset
@@ -215,53 +221,57 @@ class Map2D:
         if (coord.x == minx) or (coord.x == maxx) or (coord.y == miny) or (coord.y == maxy):
             return True
         return False
-    
+
     def count(self, value):
-        return sum(1 for v in self.map.values() if v == value)  
-    
+        return sum(1 for v in self.map.values() if v == value)
+
     def find(self, value):
-        for k,v in self.map.items():
+        for k, v in self.map.items():
             if v == value:
                 return k
         return None
 
-def rectangle_coords(x,y,width,height):
-    for y1 in range(y, y+height):
-        for x1 in range(x, x+width):            
-            yield Coord2D(x1,y1)
 
-def surrounding_coords(x,y):
+def rectangle_coords(x, y, width, height):
+    for y1 in range(y, y+height):
+        for x1 in range(x, x+width):
+            yield Coord2D(x1, y1)
+
+
+def surrounding_coords(x, y):
     return [Coord2D(x + i.x, y + i.y) for i in SURROUNDING]
-    
-def neighbor_coords(x,y):
+
+
+def neighbor_coords(x, y):
     return [Coord2D(x + i.x, y + i.y) for i in NEIGHBORS]
 
 
 def build_min_distances(valid_coords):
     distances = {}
     for coord in valid_coords:
-        distances[(coord,coord)] = 0
+        distances[(coord, coord)] = 0
         for n in coord.neighbor_coords():
             if n in valid_coords:
                 distances[(coord, n)] = 1
-    
+
     for k in valid_coords:
         for i in valid_coords:
             for j in valid_coords:
-                if (i,k) in distances:
-                    dist_ik = distances[(i,k)]
+                if (i, k) in distances:
+                    dist_ik = distances[(i, k)]
                 else:
                     continue
-                if (k,j) in distances:
-                    dist_kj = distances[(k,j)]
+                if (k, j) in distances:
+                    dist_kj = distances[(k, j)]
                 else:
                     continue
-                if (i,j) in distances:
-                    dist_ij = min(distances[i,j], dist_ik + dist_kj)
+                if (i, j) in distances:
+                    dist_ij = min(distances[i, j], dist_ik + dist_kj)
                 else:
                     dist_ij = dist_ik + dist_kj
-                distances[(i,j)] = dist_ij
+                distances[(i, j)] = dist_ij
     return distances
+
 
 def find_distances_from_coord(valid_coords, coord):
     distances = {}
@@ -281,4 +291,4 @@ def find_distances_from_coord(valid_coords, coord):
 
 class InfiniteGrid(Map2D):
     def __init__(self, default_value):
-        super().__init__(default_value)        
+        super().__init__(default_value)
