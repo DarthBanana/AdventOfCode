390cca098a1fdc0e56fe9ccf0a1b521e8b1bdd4b
Randy Aull <randyau@microsoft.com>
2023-03-18 20:38:55 -0700

    Yet another 2019 day 18 update

--- Python/2019/18/solution.py
+++ Python/2019/18/solution.py
@@ -20,6 +20,11 @@ def get_keys_from_set(keys):
 
     return key_bits
 
+def set_from_keys(keys):
+    key_set = set()
+    for i in range(26):
+        if keys & (1 << i):
+            key_set.add(chr(ord("a") + i))
 
 def get_pois_for_map(map):
         keys = set()
@@ -36,6 +41,32 @@ def get_pois_for_map(map):
             if value in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
                 doors.add(value)            
         return (pois, keys, doors)
+
+def get_key_paths_from_point(point, pois, map):
+    pos = pois[point]
+    paths = {}
+
+    dist = 0
+    queue = deque()
+    history = {}
+    queue.append((pos, dist))
+    while (len(queue)):
+        (pos, dist) = queue.popleft()
+        #if pos in history:
+        #    continue
+        #history[pos] = dist
+        value = map[pos]
+        if value != "." and value != point:
+            paths[value] = dist
+            continue
+        for n in pos.neighbors():
+            if (map[n] == None):
+                print(pos, n)
+                assert(False)
+            if map[n] == "#":
+                continue
+            queue.append((n, dist+1))
+    return paths
 
 def get_paths_from_point(point, pois, map):
     pos = pois[point]
@@ -73,6 +104,52 @@ def build_reduced_graph(map, pois):
             reduced_graph.add_edge(p, d, weight=paths[d])
 
     return reduced_graph
+
+def build_paths(map, pois, keys, doors):    
+    graph = build_reduced_graph(map, pois)
+    all_paths = {}
+    for p in pois:
+        if p in doors:
+            continue
+        for k in keys:
+            path_list = []
+            if k == p:
+                continue
+            if not nx.has_path(graph, p, k):
+                continue
+            
+            paths = nx.all_simple_paths(graph, p, k)
+            for path in paths:                
+                keys_have = 0
+                keys_needed = 0
+                length = 0
+                for v in path:
+                    if v == p:
+                        last = v
+                        continue
+                    
+                    length += graph[last][v]["weight"]     
+                    last = v               
+                    if v in keys:
+                        keys_have |= get_key_bit(v)
+                    if v in doors:
+                        if keys_have & get_door_bit(v) == 0:
+                            keys_needed |= get_door_bit(v)
+                path_list.append((length, keys_needed))
+            path_list.sort(key=lambda x: x[0])
+            all_paths[(p, k)] = path_list
+                
+
+    return all_paths
+
+def build_full_graph(map):
+    graph = nx.Graph()        
+    for coord in map:
+        if map[coord] != "#":
+            for n in coord.neighbor_coords():
+                if map[n] != "#":
+                    graph.add_edge(coord, n)
+    return graph
 
 class SubMap:
     
@@ -155,7 +232,7 @@ class Puzzle(AoCPuzzle):
         history = {}
         queue = deque()
         print(reduced_graph)
-        queue.append(((0,1,2,3), keys_left, 0, []))        
+        queue.append((("0","1","2","3"), keys_left, 0, []))        
         best_steps = 10000000000
         best_path = None
         while(len(queue)):
@@ -303,8 +380,9 @@ class Puzzle(AoCPuzzle):
             self.map[n] = "#"
         num = 0
         for d in [NW, SW, NE, SE]:
-            self.map[center + d] = num
+            self.map[center + d] = str(num)
             num += 1
+        self.get_pois()
         reduced_graph = build_reduced_graph(self.map, self.points_of_interest)
         (steps,path) = self.bfs_part2b(reduced_graph)
         return steps
@@ -373,15 +451,82 @@ class Puzzle(AoCPuzzle):
             self.map[n] = "#"
         num = 0
         for d in [NW, SW, NE, SE]:
-            self.map[center + d] = num
+            self.map[center + d] = str(num)
             num += 1
         reduced_graph = build_reduced_graph(self.map, self.points_of_interest)
         (steps,path) = self.bfs_part2d(reduced_graph)
         return steps        
+    
+    def bfs_part2d(self, all_paths):
+        keys_left = get_keys_from_set(self.keys)        
+        history = {}
+        queue = deque()        
+        queue.append((("0","1","2","3"), keys_left, 0, []))        
+        best_steps = 10000000000
+        best_path = None
+        while(len(queue)):
+            (poi, keys_left, steps, path) = queue.popleft()            
+            if keys_left == 0:  
+                if steps < best_steps:
+                    best_steps = steps
+                    best_path = path
+                continue
+            # this makes the last test case fail, but reduces the time 
+            # significantly
+            #if keys_left in history:
+            #    if history[keys_left] <= steps:
+            #        continue
+            history[keys_left] = steps
+            if (poi, keys_left) in history:
+                if history[(poi, keys_left)] <= steps:
+                    continue
+            history[(poi, keys_left)] = steps
+            
+            for k in self.keys:
+                if get_key_bit(k) & keys_left == 0:
+                    continue
+
+                for i in range(4):
+                    if not (poi[i], k) in all_paths:
+                        continue
+
+                    paths = all_paths[(poi[i], k)]
+                    for p in paths:
+                        if p[1] & keys_left == 0:
+                            new_keys_left = keys_left ^ get_key_bit(k)
+                            new_poi = list(poi)
+                            new_poi[i] = k
+                            new_poi = tuple(new_poi)
+
+                            queue.append((new_poi, new_keys_left, steps + p[0], path + [(i,k)]))  
+                            break                                        
+
+        return (best_steps, best_path)        
+
+
+
+    def part2e(self):
+        # 10/100 seconds, uses different graph type
+        self.get_pois()                
+        center = self.points_of_interest["@"]
+        self.map[center] = "#"
+        for n in center.neighbors():
+            self.map[n] = "#"
+        num = 0
+        for d in [NW, SW, NE, SE]:
+            self.map[center + d] = str(num)
+            num += 1
+        print("Building paths")
+        self.get_pois()
+        all_paths = build_paths(self.map, self.points_of_interest, self.keys, self.doors)
+        print("RUNNING")
+        (steps,path) = self.bfs_part2d(all_paths)
+        print(steps, path)
+        return steps
 
     def part2(self):
         #1816
-        return self.part2d()
+        return self.part2e()
         
 
 
